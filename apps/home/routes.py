ec6b151d0ba80ce46e60f8008125634962888536
nelsongomes11 <nelson.gomes22@hotmail.com>
2024-05-31 12:41:33 +0200

    Shop and product details done, need the add to cart and show sizes dinamically

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -8,7 +8,7 @@ from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
 from apps.home.models import Product
-
+from apps.home.models import Category
 
 @blueprint.route('/index')
 @login_required
@@ -21,8 +21,25 @@ def index():
 def shop():
         print("teste2")
         products = Product.get_all()
+        categories = Category.get_all()
+        print(products[0].Image)
         print(products)  # Check the products being retrieved
-        return render_template('home/shop.html', segment='shop', products=products)
+        return render_template('home/shop.html', segment='shop', products=products, categories=categories)
+
+@blueprint.route('/shop/product/<int:product_id>')
+@login_required
+def product_details(product_id):
+    try:
+        product = Product.get_by_id(product_id)
+        print(product)
+        if product is None:
+            return render_template('home/page-404.html'), 404
+        return render_template('home/product_details.html', product=product)
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+    except Exception as e:
+        print(e)
+        return render_template('home/page-500.html'), 500
 
 
 @blueprint.route('/<template>')
