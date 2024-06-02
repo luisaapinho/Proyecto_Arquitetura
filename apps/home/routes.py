acc0ea12928e32a15ae76040b850b198d9141f02
nelsongomes11 <nelson.gomes22@hotmail.com>
2024-06-02 14:57:33 +0200

    teste

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -4,8 +4,7 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from apps.home import blueprint
-from flask import render_template, request, redirect, url_for, flash, current_app
-from apps.authentication.models import Users
+from flask import render_template, request, Response,redirect, url_for
 from flask_login import login_required,current_user
 from jinja2 import TemplateNotFound
 from apps.home.models import Product
@@ -16,56 +15,13 @@ from apps.home.models import Cart
 from apps import db
 
 @blueprint.route('/index')
+@login_required
 def index():
-    return render_template('home/index.html', segment='index')
-
-@blueprint.route('/profile')
-@login_required
-def profile():
-    return render_template('home/profile.html', user=current_user)
-
-@blueprint.route('/update_username', methods=['POST'])
-@login_required
-def update_username():
-    new_username = request.form['username']
-    if new_username:
-        user_id = current_user.id
-        if Users.updateName(user_id, username=new_username):
-            flash('Username updated successfully!', 'success')
-        else:
-            flash('Failed to update username.', 'danger')
-    return redirect(url_for('home_blueprint.profile'))
-
-@blueprint.route('/update_email', methods=['POST'])
-@login_required
-def update_email():
-    new_email = request.form['email']
-    if new_email:
-        user_id = current_user.id
-        if Users.updateEmail(user_id, email=new_email):
-            flash('Email updated successfully!', 'success')
-        else:
-            flash('Failed to update email.', 'danger')
-    return redirect(url_for('home_blueprint.profile'))
-
-@blueprint.route('/update_password', methods=['POST'])
-@login_required
-def update_password():
-    old_password = request.form['old_password']
-    new_password = request.form['new_password']
-    
-    if old_password and new_password:
-        user = current_user
-        if Users.update_password(user, old_password, new_password):
-            flash('Password updated successfully!', 'success')
-        else:
-            flash('Old password is incorrect.', 'danger')
-    
-    return redirect(url_for('home_blueprint.profile'))
     print("teste1")
     return render_template('home/index.html', segment='index')
 
 @blueprint.route('/shop')
+@login_required
 def shop():
         print("teste2")
         products = Product.get_all()
@@ -75,6 +31,7 @@ def shop():
         return render_template('home/shop.html', segment='shop', products=products, categories=categories)
 
 @blueprint.route('/shop/product/<int:product_id>')
+@login_required
 def product_details(product_id):
     try:
         print(f"Fetching details for product_id: {product_id}")
@@ -163,7 +120,7 @@ def cart():
 def delete_item_cart(id):
     print(id)
     Cart.delete(id)
-    return redirect(url_for('home_blueprint.cart'))
+    return redirect(url_for('home_blueprint.delete_item_cart'))
 
 
 
@@ -179,21 +136,36 @@ def delete_item_cart(id):
 @blueprint.route('/<template>')
 @login_required
 def route_template(template):
+
     try:
+
         if not template.endswith('.html'):
             template += '.html'
+
+        # Detect the current page
         segment = get_segment(request)
+
+        # Serve the file (if exists) from app/templates/home/FILE.html
         return render_template("home/" + template, segment=segment)
+
     except TemplateNotFound:
         return render_template('home/page-404.html'), 404
+
     except:
         return render_template('home/page-500.html'), 500
 
+
+# Helper - Extract current page name from request
 def get_segment(request):
+
     try:
+
         segment = request.path.split('/')[-1]
+
         if segment == '':
             segment = 'index'
+
         return segment
+
     except:
         return None
